ba6da78f888755c8399d2496ec48fef4be5cda74
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-04-14 17:43:04 +0300

    Верстка ячейки для трекера с кнопкой +

---
+++ Tracker/Models/TrackerView.swift
@@ -0,0 +1,13 @@
+//
+//  TrackerView.swift
+//  Tracker
+//
+//  Created by Суворов Дмитрий Владимирович on 14.04.2023.
+//
+
+struct TrackerView {
+    let name: String
+    let color: String
+    let emoji: String
+    let daysCompleted: String
+}
--- Tracker/Utils/UIColor+Common.swift
+++ Tracker/Utils/UIColor+Common.swift
@@ -21,3 +21,20 @@ extension UIColor {
         UIColor(named: dsColor.rawValue) ?? .clear
     }
 }
+
+extension UIColor {
+    static func colorFromHex(hexString: String) -> UIColor? {
+        var hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        
+        guard hex.count == 6 else { return nil }
+        
+        var rgbValue: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&rgbValue)
+        
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgbValue & 0x0000FF) / 255.0
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+}
