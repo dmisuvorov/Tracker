01fc106202ce7368d9ddef0596d5bb4a9b8f88f6
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-06-01 20:03:25 +0300

    Интеграция библиотеки YandexMobileMetrica для аналитики

---
+++ Tracker/Services/AnalyticsService.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsService.swift
+//  Tracker
+//
+//  Created by Суворов Дмитрий Владимирович on 01.06.2023.
+//
+import YandexMobileMetrica
+
+final class AnalyticsService {
+    static let shared = AnalyticsService()
+    
+    private init() { }
+    
+    func activate() {
+        
+    }
+}
