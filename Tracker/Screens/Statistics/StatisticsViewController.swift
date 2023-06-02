9eb04fef9040f94240922a7ad755c3b3021ac78b
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-06-02 18:34:08 +0300

    Плейсхолдер для экрана статистики

--- Tracker/Screens/Statistics/StatisticsViewController.swift
+++ Tracker/Screens/Statistics/StatisticsViewController.swift
@@ -8,5 +8,42 @@
 import UIKit
 
 final class StatisticsViwController : UIViewController {
+    private lazy var emptyStatisticsPlaceholderView: UIView = {
+        let placeHolderLabel = UILabel()
+        placeHolderLabel.font = UIFont.systemFont(ofSize: 12)
+        placeHolderLabel.textAlignment = NSTextAlignment.center
+        placeHolderLabel.text = "Анализировать пока нечего"
+        let placeHolderImage = UIImageView(image: UIImage(named: "CrySmileIll"))
+        
+        let stack = UIStackView()
+        stack.axis = NSLayoutConstraint.Axis.vertical
+        stack.spacing = 8
+        stack.alignment = UIStackView.Alignment.center
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.addArrangedSubview(placeHolderImage)
+        stack.addArrangedSubview(placeHolderLabel)
+        stack.isHidden = true
+        return stack
+    }()
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        showEmptyStatisticsPlaceholder()
+    }
+    
+    
+    private func configureUI() {
+        view.backgroundColor = UIColor.dsColor(dsColor: DSColor.white)
+        view.addSubview(emptyStatisticsPlaceholderView)
+        NSLayoutConstraint.activate([
+            emptyStatisticsPlaceholderView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            emptyStatisticsPlaceholderView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+    }
+    
+    private func showEmptyStatisticsPlaceholder() {
+        emptyStatisticsPlaceholderView.isHidden = false
+    }
 }
---
+++ Tracker/Screens/Statistics/StatisticsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  StatisticsViewModel.swift
+//  Tracker
+//
+//  Created by Суворов Дмитрий Владимирович on 02.06.2023.
+//
+
+final class StatisticsViewModel {
+    
+    @Observable
+    private (set) var isPlaceholderViewHidden = true
+}
